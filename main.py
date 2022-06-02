44bcef134bff4a873fb4e9bfe0e68a27c528746e
noa450 <noa450@gmail.com>
2022-06-02 19:22:01 +0300

    5

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     df_after = utils.preprocess1(data)
     df_after = utils.preprocess2(data)
     df_after = utils.preprocess3(data)
+    utils.preprocess4(data)
     a = 1
     vals=df_after['labels0'].value_counts()
     print(vals)
