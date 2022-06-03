a2c7e9f0b9d0c0044d725fd069b734ba6c9dd836
Bar Melinarskiy <bar246802@gmail.com>
2022-06-03 08:29:42 +0300

    model

--- main.py
+++ main.py
@@ -25,70 +25,46 @@ def bar_plot(y_true, y_pred):
     fig.tight_layout()
     plt.show()
 
-
-def part1(file_name: str, is_test: bool = False):
+if __name__ == '__main__':
     try:
+        # train the model
+        file_name = 'Mission 2 - Breast Cancer/train.feats.csv'
         data = utils.load_data(filename=file_name)
+        file_name0 = 'Mission 2 - Breast Cancer/train.labels.0.csv'
+        labels0 = utils.load_labels(filename=file_name0)
+        file_name1 = 'Mission 2 - Breast Cancer/train.labels.1.csv'
+        labels1 = utils.load_labels(filename=file_name1)
 
-        if is_test:
-            data_after = utils.preprocess(data)
-        else:
-            file_name0 = 'Mission 2 - Breast Cancer/train.labels.0.csv'
-            labels0 = utils.load_labels(filename=file_name0)
-            data = utils.load_data(filename=file_name)
-            data['labels0'] = labels0['labels']
-            data_after = utils.preprocess(data)
-            new_labels = utils.preprocess_labels_part_1(df_after)
-
+        data['labels0'] = labels0['labels']
+        data['labels1'] = labels1['labels']
+        df_after = utils.preprocess(data)
+        new_labels = utils.preprocess_labels_part_1(df_after)
+        labels_1 = df_after['labels1']
         relevant_features = ["Age", "KI67_protein", "Surgery_sum", "Tumor_depth", "Tumor_width", "Margin_Type"]
-        X = data_after[relevant_features]
+        df_after = df_after[relevant_features]
         weights = np.where((new_labels >= 1).any(axis=1), 0.9, 0.05)
         model_1 = Estimator(weights=weights)
+        model_1.fit(df_after, new_labels)
+        y_pred = model_1.predict(df_after)
+        bar_plot(new_labels, y_pred)
 
-        if is_test:
-            data_after = utils.preprocess(data)
-        else:
-
+        # Get labels for test
+        file_name = 'Mission 2 - Breast Cancer/test.feats.csv'
+        test_data = utils.load_data(filename=file_name)
+        test_df = utils.preprocess(test_data)
+        test_df = test_df[relevant_features]
+        test_y_pred = model_1.predict(test_df)
+        part_1_path = 'part1/predictions.csv'
+        filepath = Path(part_1_path)
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        df_to_save = pd.DataFrame(test_y_pred)
+        df_to_save.to_csv(filepath)
     except ValueError:
         raise ValueError("Oh No - something went wrong in part 1")
 
 
-if __name__ == '__main__':
-    file_name0 = 'Mission 2 - Breast Cancer/train.labels.0.csv'
-    labels0 = utils.load_labels(filename=file_name0)
-    file_name1 = 'Mission 2 - Breast Cancer/train.labels.1.csv'
-    labels1 = utils.load_labels(filename=file_name1)
-    file_name = 'Mission 2 - Breast Cancer/train.feats.csv'
 
-    data = utils.load_data(filename=file_name)
-    data['labels0'] = labels0['labels']
-    data['labels1'] = labels1['labels']
-    df_after = utils.preprocess(data)
-    new_labels = utils.preprocess_labels_part_1(df_after)
-    labels_1 = df_after['labels1']
-    relevant_features = ["Age", "KI67_protein", "Surgery_sum", "Tumor_depth", "Tumor_width", "Margin_Type"]
-    df_after = df_after[relevant_features]
-    # weights = np.zeros(df_after.shape[0])
-    weights = np.where((new_labels >= 1).any(axis=1), 0.9, 0.05)
-    model_1 = Estimator(weights=weights)
-    model_1.fit(df_after, new_labels)
-    y_pred = model_1.predict(df_after)
-    # loss = model_1.loss(df_after, new_labels)
-    # bar_plot(new_labels, y_pred)
-    num_of_lines = np.count_nonzero((y_pred >= 1).any(axis=1))
-    num_overall = np.count_nonzero(y_pred >= 1)
 
-    # Get labels for test
-    file_name = 'Mission 2 - Breast Cancer/test.feats.csv'
-    test_data = utils.load_data(filename=file_name)
-    test_df = utils.preprocess(test_data)
-    test_df = test_df[relevant_features]
-    test_y_pred = model_1.predict(test_df)
-    part_1_path = 'part1/predictions.csv'
-    filepath = Path(part_1_path)
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    df_to_save = pd.DataFrame(test_y_pred)
-    df_to_save.to_csv(filepath)
     # est = Estimator2
     # est.fit(X=df_after, y=data['labels1'])
     # print(est.loss(df_after, data['labels1']))
